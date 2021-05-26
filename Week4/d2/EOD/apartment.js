ad2076c5c3d66d9af37716dc354c403e96b22fe4
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2021-05-25 17:03:05 -0700

    step 3

--- Week4/d2/EOD/apartment.js
+++ Week4/d2/EOD/apartment.js
@@ -14,6 +14,27 @@ class Apartment {
     this.bedrooms = bedrooms;
     this.baths = baths;
   }
+
+  apptDescription() {
+    console.log(`This fabulous ${this.bedrooms} bedroom apartment, 
+    located at ${this.address} 
+    has ${this.baths} luxuriant bathrooms 
+    for all your evacuating needs.`);
+  }
+
+  static roomCompare(bedroom1, bedroom2) {
+    if (bedroom1.bedrooms > bedroom2.bedrooms) {
+      return bedroom1;
+    } else if (bedroom2.bedrooms > bedroom1.bedrooms) {
+      return bedroom2;
+    } else {
+      return "both appartments have same number of rooms";
+    }
+  }
 }
-let appt = new Apartment();
-console.log("apartment", appt);
+
+let appt1 = new Apartment("121 address dr", 3, 4);
+
+let appt2 = new Apartment("123 code dr", 6, 4);
+
+console.log(Apartment.roomCompare(appt1, appt2));
