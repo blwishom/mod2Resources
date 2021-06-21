d574a355be1a9ccb6d09f51d1b0329ee9909c0c9
Kristen Chauncey <kristen.michelle82@gmail.com>
2021-06-21 14:54:55 -0500

    correct dry starter

--- Week4/d1-basic-coding-principles/practices/DRY-practice/DRY-code-practice.js
+++ Week4/d1-basic-coding-principles/practices/DRY-practice/DRY-code-practice.js
@@ -1,34 +1,15 @@
-//!!START SILENT
-function findBigNum(num1, num2) {
-  let bigNum = num1 > num2 ? num1 : num2;
-
-  return bigNum;
-}
-//!!END
-
 function multiplyBigNumByTwo(num1, num2) {
-  //!!START SILENT
-  let bigNum = findBigNum(num1, num2);
-  return bigNum * 2;
-  //!!END
-  //!!ADD
-let bigNum;
-if (num1 > num2) {
-  bigNum = num1;
-  return bigNum * 2;
-} else {
-  bigNum = num2;
-  return bigNum * 2;
-}
-  //!!END_ADD
+  let bigNum;
+  if (num1 > num2) {
+    bigNum = num1;
+    return bigNum * 2;
+  } else {
+    bigNum = num2;
+    return bigNum * 2;
+  }
 }
 
 function divideBigNumByThree(num1, num2) {
-  //!!START SILENT
-  let bigNum = findBigNum(num1, num2);
-  return bigNum / 3;
-  //!!END
-  //!!ADD
   let bigNum;
   if (num1 > num2) {
     bigNum = num1;
@@ -37,15 +18,9 @@ function divideBigNumByThree(num1, num2) {
     bigNum = num2;
     return bigNum / 3;
   }
-  //!!END_ADD
 }
 
 function eatMostTacos(sum1, sum2) {
-  //!!START SILENT
-  let bigNum = findBigNum(sum1, sum2);
-  return `I ate ${bigNum} tacos.`;
-  //!!END
-  //!!ADD
   let bigNum;
   if (sum1 > sum2) {
     bigNum = sum1;
@@ -54,15 +29,9 @@ function eatMostTacos(sum1, sum2) {
     bigNum = sum2;
     return `I ate ${bigNum} tacos.`;
   }
-  //!!END_ADD
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  //!!START SILENT
-  let smallDog = findBigNum(weight1, weight2) == weight1 ? weight2 : weight1;
-  return `I adopted a dog that weighs ${smallDog} pounds.`;
-  //!!END
-  //!!ADD
   let smallDog;
   if (weight1 < weight2) {
     smallDog = weight1;
@@ -71,5 +40,4 @@ function adoptSmallerDog(weight1, weight2) {
     smallDog = weight2;
     return `I adopted a dog that weighs ${smallDog} pounds.`;
   }
-  //!!END_ADD
 }
