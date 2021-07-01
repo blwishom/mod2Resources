a404f66593252d57299c6a1de71b53462b540071
Kristen <kristen.michelle82@gmail.com>
2021-07-01 14:50:24 -0500

    add ht solution

---
+++ Week5/d4-hash-tables-and-sets/NOTES/hash-tables.md
@@ -0,0 +1,11 @@
+# Hash Tables
+
+Hash tables are all around you. You've actually been using them all along. JavaScript Arrays are 
+actually hash tables under the hood. That is why they are able to take in multiple types of data in
+one array.
+
+Hash tables store indexed key/value pairs.
+
+Hash tables utilize a hashing function to generate an integer index to store your data. 
+
+A hashing function will convert keys to integers to create the key by mapping 
--- Week5/d4-hash-tables-and-sets/Practices/hash-tables-practice-master/hash-table.js
+++ Week5/d4-hash-tables-and-sets/Practices/hash-tables-practice-master/hash-table.js
@@ -1,4 +1,4 @@
-const sha256 = require('js-sha256');
+const sha256 = require("js-sha256");
 
 // Do not change this
 class KeyValuePair {
@@ -10,24 +10,44 @@ class KeyValuePair {
 }
 
 class HashTable {
-
   constructor(numBuckets = 4) {
     // fill this in
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
   }
 
   hash(key) {
     // fill this in
+    //first 8 chars of sha256 hash
+    const hashHexString = sha256(key).slice(0, 8);
+    //convert to integer
+    return parseInt(`0x${hashHexString}`);
   }
 
   hashMod(key) {
     // fill this in
+    //get the index after hashing
+    return this.hash(key) % this.capacity;
   }
 
   insert(key, value) {
     // fill this in
+    const index = this.hashMod(key);
+
+    let currentPair = this.data[index] //this.data[index] == this.head
+    while (currentPair && currentPair.key !== key) {
+      currentPair = currentPair.next;
+    }
+    if (currentPair) {
+      currentPair.value = value;
+    } else {
+      const newPair = new KeyValuePair(key, value)
+      newPair.next = this.data[index];
+      this.data[index] = newPair;
+      this.count++;
+    }
   }
-
 }
-
 
 module.exports = HashTable;
